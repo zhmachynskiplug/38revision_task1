53e89bdb73ce2146a2efecdf4e921e7c00fe2996
zhmachynskiplug <lack05@mail.ru>
2026-02-04 18:10:02 +0300

    did 3

--- task3/logic.cpp
+++ task3/logic.cpp
@@ -1,7 +1,18 @@
 #include "logic.h"
 
 bool is_power_too(int number) {
+    if (number <= 0) {
+        return false;
+    }
 
+    
+    for (int n = number; n > 1; n /= 2) {
+        
+        if (n % 2 != 0) {
+            return false;
+        }
+    }
 
-	return false;
+    return true;
 }
+
--- task3/main.cpp
+++ task3/main.cpp
@@ -8,5 +8,15 @@ int main() {
 
 	system("cls");
 
+
+	string result = is_power_too(number) == 1 ? "is" : "is not";
+
+	cout << "Your number  " << result << " power of two!" << endl;
+
+	if (is_power_too(number) == 1) {
+		cout << "Great Job! \n Programm works correctly!!!!!!!  \n" ;
+	}
+	system("pause");
+
 	return 0;
 }
